1a7e557a56dc57e185bf7f0a060b18941bb75224
lee-espiritu <130622275+lee-espiritu@users.noreply.github.com>
2023-11-14 10:17:31 +1100

    Services and Skills page and page turning

--- script.js
+++ script.js
@@ -20,3 +20,48 @@ document.addEventListener('click', function(e) {
     setTimeout(() => ping.remove(), 600); // 600ms is the animation duration
   });
   //=============================================================================
+  
+// Set initial z-index values
+const pages = document.querySelectorAll('.book-page');
+pages.forEach((page, index) => {
+  // Assuming the profile page is the first one
+  if (index === 0) { // if it's the profile page
+    page.style.zIndex = pages.length;
+  } else {
+    page.style.zIndex = pages.length - index; // Set z-index in descending order for other pages
+  }
+});
+
+//turn pages when click next or prev button
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+
+pageTurnBtn.forEach((el, index) => {
+    el.onclick = () => {
+        const pageTurnId = el.getAttribute('data-page');
+        const pageTurn = document.getElementById(pageTurnId);
+
+        if (pageTurn.classList.contains('turn')) {
+            pageTurn.classList.remove('turn');
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 - index;
+            }, 500)
+        }
+        else {
+            pageTurn.classList.add('turn');
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 + index;
+            }, 500)
+        }
+    }
+})
+
+// Ensure this code is below your existing event listener for page turns
+window.onload = () => {
+  // Code to show profile page and reset other pages
+  pages.forEach((page, index) => {
+    if (index !== 0) { // if it's not the profile page
+      page.classList.remove('turn'); // Remove 'turn' class if it's not the profile page
+    }
+  });
+};
+
